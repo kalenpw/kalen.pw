0c1b833f2b5fe47b833e8baf02c84c34740c3b6e
kalenpw <kalenpwilliams@gmail.com>
2019-03-08 10:15:12 -0700

    Switch to routed pages and add resume link

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,35 @@
 <template>
     <div id="app">
         <navbar></navbar>
-        <banner></banner>
-        <div class="section">
-            <projects></projects>
-        </div>
-        <end></end>
+        <router-view/>
+
+        <footer>
+            <end></end>
+        </footer>
     </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import Card from "./components/Card.vue";
-import Message from "./components/Message.vue";
-import Banner from "./components/Banner.vue";
-import End from "./components/End.vue";
-import Projects from "./components/Projects.vue";
-
+import End from "@/components/End.vue";
 export default {
+    name: "app",
     components: {
         Navbar,
-        Card,
-        Message,
-        Banner,
-        End,
-        Projects
-    },
-    name: "app",
-    data() {
-        return {
-            switch_info:
-                "A place for Nintendo Switch fans to share artwork and vote on their favorite alternate art.",
-            switch_url: "http://switchart.tk",
-            switch_github: "https://github.com/kalenpw/SwitchArt",
-
-            monopoly_info:
-                "Peer to peer Monopoly in C# for my Advanced Software Engineering Project class.",
-            monopoly_url: "https://github.com/CS4488/Pocatello-Landlords",
-
-            cmus_info:
-                "A browser based remote written in Nodejs and an Android app for CMus music player",
-            cmus_url1: "https://github.com/kalenpw/CmusRemoteBrowser",
-            cmus_url2: "https://github.com/kalenpw/CmusRemoteAndroid",
-
-            homelab_info:
-                "Homelab Server Dell PowerEdge r710 I setup and maintain. Several Nginx Vhosts, Emby Media Server, SSHFS,",
-            homelab_url: "/"
-        };
+        End
     }
 };
 </script>
 
-<style scoped>
-div {
-    /* background-color: #222; */
-    /* height: 100vh; */
+<style>
+html{
+  /* min-height: 100%; */
+
+}
+footer {
+    /* position: fixed; */
+    /* bottom: 0; */
+    /* width: 100%; */
+    /* overflow: hidden; */
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,18 +2,40 @@
     <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
             <a class="navbar-item is-size-4" href="/">Kalen Williams</a>
-
-            <!-- <a
+            
+            <a
                 role="button"
                 class="navbar-burger burger"
                 aria-label="menu"
                 aria-expanded="false"
                 data-target="navbarBasicExample"
+                v-bind:class="[active ? activeClass : '']"
+                @click="toggleNav()"
             >
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
-            </a>-->
+            </a>
+        </div>
+        <div class="navbar-menu" v-bind:class="[active ? activeClass : '']">
+            <div class="navbar-start">
+                <!-- <a class="navbar-item">Documentation</a> -->
+                <!-- <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">More</a>
+
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">About</a>
+                        <a class="navbar-item">Jobs</a>
+                        <a class="navbar-item">Contact</a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">Report an issue</a>
+                    </div>
+                </div>-->
+            </div>
+
+            <div class="navbar-end">
+                <a @click="closeNav" class="navbar-item" href="/resume.pdf">Resume</a>
+            </div>
         </div>
     </nav>
 </template>
@@ -21,12 +43,23 @@
 <script>
 // import Navitem from "./Navitem.vue";
 export default {
-    components: {
-        // Navitem
+    name: "Navbar",
+    props: {
+        msg: String
     },
-    name: "navbar",
     data() {
-        return {};
+        return {
+            active: false,
+            activeClass: "is-active"
+        };
+    },
+    methods: {
+        toggleNav() {
+            this.active = !this.active;
+        },
+        closeNav() {
+            this.active = false;
+        }
     }
 };
 </script>
