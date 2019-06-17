8868e5dce78052633f4c410fc52dbf4d2c02840a
kalenpw <kalenpwilliams@gmail.com>
2019-06-17 11:24:34 -0600

    Updated Resume, remove expected date from BS

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,7 +4,7 @@
             <div class="container columns">
                 <div class="column">
                     <h1 class="title">Fullstack Developer</h1>
-                    <h2 class="subtitle">B.S. Computer Science, May 2019</h2>
+                    <h2 class="subtitle">B.S. Computer Science</h2>
                 </div>
             </div>
         </div>
