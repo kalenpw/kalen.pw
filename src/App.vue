2e8ca75713db24caa6fe71dcf90335049063a842
Kalen Williams <kwilliams@apgwest.com>
2021-05-14 15:31:09 -0600

    Add alert that information is outdated

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-    <div id="app">
-        <navbar></navbar>
-        <div class="site">
-            <router-view/>
-        </div>
-        <end></end>
+  <div id="app">
+    <alert-banner/>
+    <navbar></navbar>
+    <div class="site">
+      <router-view />
     </div>
+    <end></end>
+  </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
 import End from "@/components/End.vue";
+import AlertBanner from './components/AlertBanner.vue';
+
 export default {
-    name: "app",
-    components: {
-        Navbar,
-        End
-    }
+  name: "app",
+  components: {
+    Navbar,
+    End,
+    AlertBanner,
+  },
 };
 </script>
 
 <style scoped>
 #app {
-    display: flex;
-    min-height: 100vh;
-    flex-direction: column;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
 }
 .site {
-    flex: 1;
+  flex: 1;
 }
 </style>
