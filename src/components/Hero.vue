bd25e90a24ca44bd09baede5e8dd0c91fe7f8461
kalenpw <kalenpwilliams@gmail.com>
2021-07-01 17:22:29 -0600

    Add profile photo

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="kpw-hero container-fluid text-light mt-0 pt-5">
-        <div class="container pt-4 pb-4 pb-lg-5">
-            <h2 class="display-4 pt-4 pt-lg-5">Software Developer</h2>
-            <p class="h5">B.S. Computer Science</p>
+        <div class="container pt-4 pb-4 pb-lg-5 d-flex align-items-center">
+            <img class="rounded-circle mt-5 me-5 d-none d-md-block" src="/img/avi.jpg" alt="Kalen headshot">
+            <div>
+                <h2 class="display-4 pt-4 pt-lg-5">Software Developer</h2>
+                <p class="h5">B.S. Computer Science</p>
+            </div>
         </div>
     </div>
 </template>
@@ -23,5 +26,7 @@ export default {
         font-size: 1.1rem;
     }
 }
-
+img {
+    max-height: 200px;
+}
 </style>
