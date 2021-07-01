756b5d6a16d7b472ba0f41c1201eec05f2aa9367
kalenpw <kalenpwilliams@gmail.com>
2021-06-30 19:22:04 -0600

    Hero layout tweaks

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="kpw-hero container-fluid text-light mt-0 pt-5">
-        <div class="container pt-5 pb-3 pb-lg-5">
-            <h1 class="display-3 pt-2 pt-lg-5">Software Developer</h1>
-            <p class="display-6">B.S. Computer Science</p>
+        <div class="container pt-4 pb-3 pb-lg-5">
+            <h1 class="display-3 pt-4 pt-lg-5">Software Developer</h1>
+            <p class="h5">B.S. Computer Science</p>
         </div>
     </div>
 </template>
