2ebb4196b1fc9c222e78ae473b7af98bb0d37b4c
kalenpw <kalenpwilliams@gmail.com>
2021-07-03 16:05:39 -0600

    Smaller font size on hero mobile

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@
         <div class="container pt-4 pb-4 pb-lg-5 d-flex align-items-center">
             <img class="rounded-circle mt-5 me-5 d-none d-md-block" src="/img/avi.jpg" alt="Kalen headshot">
             <div>
-                <h2 class="display-4 pt-4 pt-lg-5">Software Developer</h2>
+                <h2 class="display-5 pt-4 pt-lg-5">Software Developer</h2>
                 <p class="h5">B.S. Computer Science</p>
             </div>
         </div>
@@ -21,9 +21,12 @@ export default {
     /* background: linear-gradient(45deg, #00d1b2, #06e6c4); */
     background-color: #00d1b2;
 }
-@media only screen and (max-width: 1000px) {
+@media only screen and (max-width: 767px) {
     .h5 {
         font-size: 1.1rem;
+    }
+    h2 {
+        font-size: 2rem;
     }
 }
 img {
